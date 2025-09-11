54b28383c67738a96487cc801dd9dc2982ecf76d
Aleksandr M <amikolyk@yandex.ru>
2025-09-11 10:39:39 +0300

    move to main directory /

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -19,10 +19,10 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://jt-lab-com.github.io',
+  url: 'https://docs.jt-lab-com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/docs/',
+  baseUrl: '/',
   
 
   // GitHub pages deployment config.
